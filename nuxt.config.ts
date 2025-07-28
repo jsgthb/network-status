d916ee00dc4920b3a53563b7640d5eaa8a1b1539
jsgthb <jeff.schaack@gmail.com>
2025-07-29 00:41:47 +0200

    Installed Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@nuxt/ui',
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    '@pinia/nuxt'
   ],
   nitro: {
     experimental: {
